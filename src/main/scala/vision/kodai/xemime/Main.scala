36d724faf06cd892b243cc47fe9d2979da170801
0918nobita <nobita.0918@gmail.com>
2020-06-22 20:46:32 +0900

    :white_check_mark: Cats の State に関するテストを追加

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -1,11 +1,7 @@
 package vision.kodai.xemime
 
-import cats.data.State
-
 object Main {
   def main(args: Array[String]) {
-    val state  = State[Int, Int](s => (s + 1, s * 2))
-    val state2 = state.flatMap(_ => state)
-    println(state2.run(1).value) // => (3, 4)
+    println("Hello, world!")
   }
 }
---
+++ src/test/scala/CatsTest.scala
@@ -0,0 +1,10 @@
+import cats.data.State
+import org.scalatest.funsuite.AnyFunSuite
+
+class CatsTest extends AnyFunSuite {
+  test("State") {
+    val state  = State[Int, Int](s => (s + 1, s * 2))
+    val state2 = state.flatMap(_ => state)
+    assert(state2.run(1).value == (3, 4))
+  }
+}
