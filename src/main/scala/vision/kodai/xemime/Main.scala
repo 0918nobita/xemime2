cb8a15858026464c21ea23a6acfd5a3d9b5ac310
0918nobita <nobita.0918@gmail.com>
2020-06-22 19:21:03 +0900

    :truck: Option.getOrElse に関するサンプルをテスト側に移動

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -3,16 +3,7 @@ package vision.kodai.xemime
 import cats.data.State
 
 object Main {
-  def maybeItWillReturnSomething(cond: Boolean): Option[String] = {
-    if (cond) Some("Found value") else None
-  }
-
-  def main(args: Array[String]): Unit = {
-    val value1 = maybeItWillReturnSomething(true)
-    val value2 = maybeItWillReturnSomething(false)
-    println(value1.getOrElse("Placeholder")) // => Found value
-    println(value2.getOrElse("Placeholder")) // => Placeholder
-
+  def main(args: Array[String]) {
     val someValue: Option[Double] = Some(20.0)
     val doubleValue = someValue match {
       case Some(v) => v
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,7 +1,13 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  test("Calc") {
-    assert(10 == 5 * 2)
+  test("Option.getOrElse") {
+    def maybeItWillReturnSomething(cond: Boolean): Option[String] =
+      if (cond) Some("Found value") else None
+
+    val value1 = maybeItWillReturnSomething(true)
+    val value2 = maybeItWillReturnSomething(false)
+    assert(value1.getOrElse("No value") == "Found value")
+    assert(value2.getOrElse("No value") == "No value")
   }
 }
