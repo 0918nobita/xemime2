1103412d6451d6e155ad54bdde733453e3bdd11f
0918nobita <nobita.0918@gmail.com>
2020-06-23 00:26:26 +0900

    :white_check_mark: コンス、リストの結合に関するテストを追加

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -102,11 +102,25 @@ class Test extends AnyFunSuite {
     assert(countUp(3) == 4)
   }
 
-  test("List.map") {
-    val makeUpper = { xs: List[String] => xs map { _.toUpperCase } }
-    val actual   = makeUpper(List("a", "b", "c"))
-    val expected = List("A", "B", "C")
-    assert(actual == expected)
+  test("Cons") {
+    val list = List(1, 3, 5, 7)
+    // right-associative operator
+    assert(list.::(0) == List(0, 1, 3, 5, 7))
+    assert(-1 :: list == List(-1, 1, 3, 5, 7))
+  }
+
+  test("Concat Lists") {
+    val forward  = List(1, 3)
+    val backward = List(5, 7)
+    assert(forward ::: backward == List(1, 3, 5, 7))
+  }
+
+  test("Access values of Lists") {
+    val list = List(1, 2, 3)
+    assert(list(2) == 3)
+    assertThrows[IndexOutOfBoundsException] { list(4) }
+    assert(list.headOption.contains(1))
+    assert(list.tail == List(2, 3))
   }
 
   test("List Comparison") {
@@ -124,12 +138,11 @@ class Test extends AnyFunSuite {
     assert(listC eq listD)
   }
 
-  test("Access values of Lists") {
-    val list = List(1, 2, 3)
-    assert(list(2) == 3)
-    assertThrows[IndexOutOfBoundsException] { list(4) }
-    assert(list.headOption.contains(1))
-    assert(list.tail == List(2, 3))
+  test("List.map") {
+    val makeUpper = { xs: List[String] => xs map { _.toUpperCase } }
+    val actual   = makeUpper(List("a", "b", "c"))
+    val expected = List("A", "B", "C")
+    assert(actual == expected)
   }
 
   test("Filter Lists / Immutability") {
