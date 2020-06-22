fd268f4af77aa8df944ddc6f71eea170a70fd691
0918nobita <nobita.0918@gmail.com>
2020-06-22 19:02:22 +0900

    :sparkles: List の等値性・等価性に関するサンプルを追加

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -79,6 +79,19 @@ object Main {
     val makeUpper = { xs: List[String] => xs map { _.toUpperCase } }
     println(makeUpper(List("a", "b", "c"))) // => List(A, B, C)
 
+    val listA = List(1, 2, 3)
+    val listB = List(1, 2, 3)
+    println(listA == listB) // => true
+    println(listA eq listB) // => false
+
+    val listC: List[String] = Nil
+    val listD: List[Int]    = Nil
+    println(listC == Nil)   // => true
+    println(listD == Nil)   // => true
+    println(listC eq Nil)   // => true
+    println(listD eq Nil)   // => true
+    println(listC eq listD) // => true
+
     val state  = State[Int, Int](s => (s + 1, s * 2))
     val state2 = state.flatMap(_ => state)
     println(state2.run(1).value) // => (3, 4)
