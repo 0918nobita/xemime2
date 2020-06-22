e07f907ae7a69ab5ceb57822027166db8fa13a92
0918nobita <nobita.0918@gmail.com>
2020-06-23 01:18:42 +0900

    :white_check_mark: Map の基本的な機能に関するテストを追加

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -171,4 +171,24 @@ class Test extends AnyFunSuite {
 
     assert(Point2.unapply(Point2(1, 2)).contains((1, 2)))
   }
+
+  test("(->) operator") {
+    // alternate syntax for key-value pair
+    assert((1, "a") == (1 -> "a"))
+  }
+
+  test("(Maps) Initialization") {
+    val myMap = Map("JA" -> "Japanese", "EN" -> "English", "ZH" -> "Chinese")
+    assert(myMap.size == 3)
+
+    val redundantMap = Map("a" -> "A", "b" -> "B", "a" -> "A", "c" -> "C")
+    assert(redundantMap.size == 3)
+  }
+
+  test("(Maps) Add elements") {
+    val myMap  = Map(1 -> "a", 2 -> "b")
+    val newMap = myMap + (3 -> "c") + (4 -> "d")
+    assert(newMap.contains(3))
+    assert(newMap.toString === "Map(1 -> a, 2 -> b, 3 -> c, 4 -> d)")
+  }
 }
