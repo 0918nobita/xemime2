8ecbf9702502e493377654ddb07969cde1bfc889
0918nobita <nobita.0918@gmail.com>
2020-06-22 13:53:56 +0900

    :sparkles: Option, getOrElse に関するサンプルを追加

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -3,11 +3,20 @@ package vision.kodai.xemime
 import cats.data.State
 
 object Main {
+  def maybeItWillReturnSomething(cond: Boolean): Option[String] = {
+    if (cond) Some("Found value") else None
+  }
+
   def main(args: Array[String]): Unit = {
     val state  = State[Int, Int](s => (s + 1, s * 2))
     val state2 = state.flatMap(_ => state)
     println(state2.run(1).value)
 
-    println(new Point(1, 2))
+    println(new Point(1, 2)) // => Point(1, 2)
+
+    val value1 = maybeItWillReturnSomething(true)
+    val value2 = maybeItWillReturnSomething(false)
+    println(value1.getOrElse("Placeholder")) // => Found value
+    println(value2.getOrElse("Placeholder")) // => Placeholder
   }
 }
