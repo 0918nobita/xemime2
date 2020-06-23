dcdb610538efaa4a7bf66a9f31bbee554c4c8eb6
0918nobita <nobita.0918@gmail.com>
2020-06-23 12:07:56 +0900

    :truck: リストに関するテストを分離

---
+++ src/test/scala/ListTest.scala
@@ -0,0 +1,59 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class ListTest extends AnyFunSuite {
+  test("Cons") {
+    val list = List(1, 3, 5, 7)
+    // right-associative operator
+    assert(list.::(0) == List(0, 1, 3, 5, 7))
+    assert(-1 :: list == List(-1, 1, 3, 5, 7))
+  }
+
+  test("Concat Lists") {
+    val forward  = List(1, 3)
+    val backward = List(5, 7)
+    assert(forward ::: backward == List(1, 3, 5, 7))
+  }
+
+  test("Access values of Lists") {
+    val list = List(1, 2, 3)
+    assert(list(2) == 3)
+    assertThrows[IndexOutOfBoundsException] { list(4) }
+    assert(list.headOption.contains(1))
+    assert(list.tail == List(2, 3))
+  }
+
+  test("List Comparison") {
+    val listA = List(1, 2, 3)
+    val listB = List(1, 2, 3)
+    assert(listA == listB)
+    assert(!(listA eq listB))
+
+    val listC: List[String] = Nil
+    val listD: List[Int]    = Nil
+    assert(listC == Nil)
+    assert(listD == Nil)
+    assert(listC eq Nil)
+    assert(listD eq Nil)
+    assert(listC eq listD)
+  }
+
+  test("List.map") {
+    val makeUpper = { xs: List[String] => xs map { _.toUpperCase } }
+    val actual   = makeUpper(List("a", "b", "c"))
+    val expected = List("A", "B", "C")
+    assert(actual == expected)
+  }
+
+  test("Filter Lists / Immutability") {
+    val list = List(4, 5, 6)
+    assert(list.filter({ _ % 2 == 0 }) == List(4, 6))
+    assert(list.filterNot({ _ % 3 == 0 }) == List(4, 5))
+    assert(list === List(4, 5, 6))
+  }
+
+  test("Reduce Lists") {
+    val list = List(1, 3, 5, 7)
+    assert(16 == list.foldLeft(z = 0)(op = (acc, elem) => acc + elem))
+    assert(26 == list.foldLeft(10) { _ + _ })
+  }
+}
