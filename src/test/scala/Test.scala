889274abe7e9bddc32cffeb426d31363d9808691
0918nobita <nobita.0918@gmail.com>
2020-06-23 12:06:20 +0900

    :truck: 関数オブジェクトに関するテストを分離

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,34 +1,6 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  test("Functions") {
-    val func = new Function[Int, Int] {
-      // implement abstract method
-      def apply(v1: Int): Int = v1 - 1
-    }
-    assert(func(10) == 9)
-    assert(func.apply(10) == 9)
-    assert(
-      func.isInstanceOf[Int => Int] // `Int => Int` == `Function1[Int, Int]`
-    )
-
-    val func2 = (x: Int) => { x * 2 }
-    assert(func2(3) == 6)
-
-    val func3 = { x: Int => x * 3 }
-    assert(func3(4) == 12)
-  }
-
-  test("Closures") {
-    var count = 0
-    val countUp = { diff: Int =>
-      count += diff
-      count
-    }
-    assert(countUp(1) == 1)
-    assert(countUp(3) == 4)
-  }
-
   test("Cons") {
     val list = List(1, 3, 5, 7)
     // right-associative operator
