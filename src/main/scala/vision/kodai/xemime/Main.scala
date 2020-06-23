e5b7f2fcc6f546769d8597e697f52e4abe57f6ae
0918nobita <nobita.0918@gmail.com>
2020-06-23 14:52:50 +0900

    :sparkles: ScalaFX でウィンドウを表示するサンプルを追加

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -1,7 +1,22 @@
 package vision.kodai.xemime
 
-object Main {
-  def main(args: Array[String]) {
-    println("hello, world!")
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.{Group, Scene}
+import scalafx.scene.text.Text
+
+object Main extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Xemime"
+    resizable = false
+    scene = new Scene(320.0, 160.0) {
+      root = new Group {
+        layoutX = 30.0
+        layoutY = 80.0
+        children = new Text("Hello") {
+          onMouseClicked = _ => println("Hello")
+        }
+      }
+    }
   }
 }
