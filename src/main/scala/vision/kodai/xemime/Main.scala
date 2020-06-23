b98a215bb52b8fa1f3dd093ad0d0452867005338
0918nobita <nobita.0918@gmail.com>
2020-06-23 11:35:47 +0900

    :truck: Monocle に関するサンプルをテスト側に移動

--- src/main/scala/vision/kodai/xemime/Main.scala
+++ src/main/scala/vision/kodai/xemime/Main.scala
@@ -1,17 +1,7 @@
 package vision.kodai.xemime
 
-import SampleLens._
-
 object Main {
   def main(args: Array[String]) {
-    val employee = Employee(Company(Address(Street("foo"))))
-    val lens =
-      _company composeLens _address composeLens _street composeLens _name
-    val name      = lens get employee
-    val employee2 = lens.set("bar")(employee)
-    val employee3 = lens.modify(_.capitalize)(employee)
-    println(name)      // => foo
-    println(employee2) // => Employee(Company(Address(Street(bar))))
-    println(employee3) // => Employee(Company(Address(Street(Foo))))
+    println("hello, world!")
   }
 }
---
+++ src/test/scala/MonocleTest.scala
@@ -0,0 +1,34 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+import monocle.Lens
+
+class MonocleTest extends AnyFunSuite {
+  test("Create / Compose / Use Lens") {
+    case class Street(name: String)
+    case class Address(street: Street)
+    case class Company(address: Address)
+    case class Employee(company: Company)
+
+    val _name = Lens[Street, String](_.name) { str => s => s.copy(name = str) }
+    val _street = Lens[Address, Street](_.street) { s => a =>
+      a.copy(street = s)
+    }
+    val _address = Lens[Company, Address](_.address) { a => c =>
+      c.copy(address = a)
+    }
+    val _company = Lens[Employee, Company](_.company) { c => e =>
+      e.copy(company = c)
+    }
+
+    val employee = Employee(Company(Address(Street("bourbon"))))
+    val lens =
+      _company composeLens _address composeLens _street composeLens _name
+    assert((lens get employee) == "bourbon")
+
+    val employee2 = lens.set("fleet")(employee)
+    assert(employee2 == Employee(Company(Address(Street("fleet")))))
+
+    val employee3 = lens.modify(_.capitalize)(employee)
+    assert(employee3 == Employee(Company(Address(Street("Bourbon")))))
+  }
+}
