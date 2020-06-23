ebcec0d4ded00e5e3bfee1e013f9e0a6af09ae65
0918nobita <nobita.0918@gmail.com>
2020-06-23 12:09:03 +0900

    :truck: Map に関するテストを分離

--- src/test/scala/MapTest.scala
+++ src/test/scala/MapTest.scala
@@ -1,12 +1,12 @@
 import org.scalatest.funsuite.AnyFunSuite
 
-class Test extends AnyFunSuite {
+class MapTest extends AnyFunSuite {
   test("(->) operator") {
     // alternate syntax for key-value pair
     assert((1, "a") == (1 -> "a"))
   }
 
-  test("(Maps) Initialization") {
+  test("Initialization") {
     val myMap = Map("JA" -> "Japanese", "EN" -> "English", "ZH" -> "Chinese")
     assert(myMap.size == 3)
 
@@ -14,7 +14,7 @@ class Test extends AnyFunSuite {
     assert(redundantMap.size == 3)
   }
 
-  test("(Maps) Add elements") {
+  test("Add elements") {
     val myMap  = Map(1 -> "a", 2 -> "b")
     val newMap = myMap + (3 -> "c") + (4 -> "d")
     assert(newMap.contains(3))
