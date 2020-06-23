a48aa4b1909fcdffe81da3ca4130563bdae7fcb4
0918nobita <nobita.0918@gmail.com>
2020-06-23 16:01:33 +0900

    :white_check_mark: RegexParser に関するテストを追加

---
+++ src/test/scala/ParsecTest.scala
@@ -0,0 +1,15 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.parsing.combinator._
+
+class ParsecTest extends AnyFunSuite {
+  test("Regex parsers") {
+    object SimpleParser extends RegexParsers {
+      def word: Parser[String] = "[a-z]+".r ^^ (_ + "!")
+    }
+
+    val result = SimpleParser.parse(SimpleParser.word, "abc7")
+    assert(result.successful)
+    assert(result.get == "abc!")
+  }
+}
