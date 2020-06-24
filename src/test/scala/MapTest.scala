915ada4e45e7245c33ba61117c73c1c4d8230bff
0918nobita <nobita.0918@gmail.com>
2020-06-24 12:42:38 +0900

    :white_check_mark: Map.values に関するテストを追加

--- src/test/scala/MapTest.scala
+++ src/test/scala/MapTest.scala
@@ -1,6 +1,8 @@
+import org.scalacheck.Prop.forAll
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.Checkers
 
-class MapTest extends AnyFunSuite {
+class MapTest extends AnyFunSuite with Checkers {
   test("(->) operator") {
     // alternate syntax for key-value pair
     assert((1, "a") == (1 -> "a"))
@@ -19,5 +21,19 @@ class MapTest extends AnyFunSuite {
     val newMap = myMap + (3 -> "c") + (4 -> "d")
     assert(newMap.contains(3))
     assert(newMap.toString === "Map(1 -> a, 2 -> b, 3 -> c, 4 -> d)")
+  }
+
+  test("Values") {
+    val myMap     = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin")
+    val mapValues = myMap.values
+    assert(mapValues.head == "Michigan")
+
+    val myMap2     = Map("OH" -> "Ohio", "MI" -> "Michigan", "WI" -> "Wisconsin")
+    val mapValues2 = myMap2.values
+    assert(mapValues2.head == "Ohio")
+
+    check(forAll { (m: Map[String, String]) =>
+      m.isEmpty || m.values.head == m.head._2
+    })
   }
 }
