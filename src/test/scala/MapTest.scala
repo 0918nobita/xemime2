1f0c28803e04c9239f4a016de5032a65633f62f6
0918nobita <nobita.0918@gmail.com>
2020-06-24 16:37:59 +0900

    :building_construction: Map.values に関するテストをプロパティベースに書き換えた

--- src/test/scala/MapTest.scala
+++ src/test/scala/MapTest.scala
@@ -1,11 +1,11 @@
-import org.scalacheck.Prop.forAll
+import org.scalacheck.{Arbitrary, Gen, Prop}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.Checkers
 
 class MapTest extends AnyFunSuite with Checkers {
   test("(->) operator") {
     // alternate syntax for key-value pair
-    check(forAll { (str: String, n: Int) => (str, n) == (str -> n) })
+    check(Prop.forAll { (str: String, n: Int) => (str, n) == (str -> n) })
   }
 
   test("Initialization") {
@@ -17,15 +17,15 @@ class MapTest extends AnyFunSuite with Checkers {
   }
 
   test("Add elements") {
-    check(forAll { (baseMap: Map[String, Int], newKey: String, newValue: Int) =>
-      val newMap = baseMap + (newKey -> newValue)
-      newMap.contains(newKey) && newMap(newKey) == newValue
+    check(Prop.forAll {
+      (baseMap: Map[String, Int], newKey: String, newValue: Int) =>
+        val newMap = baseMap + (newKey -> newValue)
+        newMap.contains(newKey) && newMap(newKey) == newValue
     })
   }
 
   test("Values") {
-    check(forAll { (m: Map[String, String]) =>
-      m.isEmpty || m.values.head == m.head._2
-    })
+    val gen = Gen.nonEmptyMap(Arbitrary.arbitrary[(String, String)])
+    check(Prop.forAll(gen) { m => m.values.head == m.head._2 })
   }
 }
