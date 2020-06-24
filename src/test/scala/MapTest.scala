7ec6d7a9a6ab0f540929fd6e90d3d95ae2057666
0918nobita <nobita.0918@gmail.com>
2020-06-24 16:07:52 +0900

    :building_construction: Map に関するテストをプロパティベースに書き換えた

--- src/test/scala/MapTest.scala
+++ src/test/scala/MapTest.scala
@@ -5,7 +5,7 @@ import org.scalatestplus.scalacheck.Checkers
 class MapTest extends AnyFunSuite with Checkers {
   test("(->) operator") {
     // alternate syntax for key-value pair
-    assert((1, "a") == (1 -> "a"))
+    check(forAll { (str: String, n: Int) => (str, n) == (str -> n) })
   }
 
   test("Initialization") {
@@ -17,21 +17,13 @@ class MapTest extends AnyFunSuite with Checkers {
   }
 
   test("Add elements") {
-    val myMap  = Map(1 -> "a", 2 -> "b")
-    val newMap = myMap + (3 -> "c") + (4 -> "d")
-    assert(newMap.contains(3))
-    assert(newMap.toString === "Map(1 -> a, 2 -> b, 3 -> c, 4 -> d)")
+    check(forAll { (baseMap: Map[String, Int], newKey: String, newValue: Int) =>
+      val newMap = baseMap + (newKey -> newValue)
+      newMap.contains(newKey) && newMap(newKey) == newValue
+    })
   }
 
   test("Values") {
-    val myMap     = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin")
-    val mapValues = myMap.values
-    assert(mapValues.head == "Michigan")
-
-    val myMap2     = Map("OH" -> "Ohio", "MI" -> "Michigan", "WI" -> "Wisconsin")
-    val mapValues2 = myMap2.values
-    assert(mapValues2.head == "Ohio")
-
     check(forAll { (m: Map[String, String]) =>
       m.isEmpty || m.values.head == m.head._2
     })
