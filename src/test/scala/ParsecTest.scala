c7e258c55ccf05baf723ddbfbab3ce5c31647dcd
0918nobita <nobita.0918@gmail.com>
2020-06-26 18:55:39 +0900

    :white_check_mark: RegexParser に関するプロパティベースのテストを追加

--- src/test/scala/ParsecTest.scala
+++ src/test/scala/ParsecTest.scala
@@ -1,14 +1,32 @@
+import org.scalacheck.{Gen, Prop}
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.Checkers
 import scala.util.parsing.combinator._
 
-class ParsecTest extends AnyFunSuite {
+class ParsecTest extends AnyFunSuite with Checkers {
   test("Regex parsers") {
     object SimpleParser extends RegexParsers {
       def word: Parser[String] = "[a-z]+".r ^^ (_ + "!")
     }
 
-    val result = SimpleParser.parse(SimpleParser.word, "abc7")
-    assert(result.successful)
-    assert(result.get == "abc!")
+    val genNelOfAlphaLowerChars =
+      Gen.nonEmptyListOf(Gen.alphaLowerChar).map(_.mkString)
+
+    val genNelOfMarks = Gen
+      .nonEmptyListOf(Gen.oneOf(Seq('!', '\"', '$', '%', '&')))
+      .map(_.mkString)
+
+    val gen = for {
+      forward  <- genNelOfAlphaLowerChars
+      backward <- genNelOfMarks
+    } yield {
+      (forward, forward + backward)
+    }
+
+    check(Prop.forAll(gen) {
+      case (expected, src) =>
+        val result = SimpleParser.parse(SimpleParser.word, src)
+        result.successful && result.get == s"$expected!"
+    })
   }
 }
