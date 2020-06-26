84542c9522c9e224640f3d35e805d10ef17e2438
0918nobita <nobita.0918@gmail.com>
2020-06-26 11:57:37 +0900

    :white_check_mark: Monocle に関するプロパティベースのテストを追加

--- src/test/scala/MonocleTest.scala
+++ src/test/scala/MonocleTest.scala
@@ -1,33 +1,64 @@
 import monocle.Lens
+import org.scalacheck.{Arbitrary, Prop}
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.Checkers
 
-class MonocleTest extends AnyFunSuite {
-  test("Create / Compose / Use Lens") {
-    case class Street(name: String)
-    case class Address(street: Street)
-    case class Company(address: Address)
-    case class Employee(company: Company)
+case class Street(name: String)
+case class Address(street: Street)
+case class Company(address: Address)
+case class Employee(company: Company)
 
-    val _name = Lens[Street, String](_.name) { str => s => s.copy(name = str) }
-    val _street = Lens[Address, Street](_.street) { s => a =>
-      a.copy(street = s)
-    }
-    val _address = Lens[Company, Address](_.address) { a => c =>
-      c.copy(address = a)
-    }
-    val _company = Lens[Employee, Company](_.company) { c => e =>
-      e.copy(company = c)
-    }
+class MonocleTest extends AnyFunSuite with Checkers {
+  import Arbitrary.arbitrary
+  import Prop.forAll
 
-    val employee = Employee(Company(Address(Street("bourbon"))))
-    val lens =
-      _company composeLens _address composeLens _street composeLens _name
-    assert((lens get employee) == "bourbon")
+  implicit val street: Arbitrary[Street] =
+    Arbitrary(arbitrary[String].map(Street))
 
-    val employee2 = lens.set("fleet")(employee)
-    assert(employee2 == Employee(Company(Address(Street("fleet")))))
+  implicit val address: Arbitrary[Address] =
+    Arbitrary(arbitrary[Street].map(Address))
 
-    val employee3 = lens.modify(_.capitalize)(employee)
-    assert(employee3 == Employee(Company(Address(Street("Bourbon")))))
+  implicit val company: Arbitrary[Company] =
+    Arbitrary(arbitrary[Address].map(Company))
+
+  implicit val employee: Arbitrary[Employee] =
+    Arbitrary(arbitrary[Company].map(Employee))
+
+  val _name: Lens[Street, String] =
+    Lens[Street, String](_.name) { str => s => s.copy(name = str) }
+
+  val _street: Lens[Address, Street] =
+    Lens[Address, Street](_.street) { s => a => a.copy(street = s) }
+
+  val _address: Lens[Company, Address] =
+    Lens[Company, Address](_.address) { a => c => c.copy(address = a) }
+
+  val _company: Lens[Employee, Company] =
+    Lens[Employee, Company](_.company) { c => e => e.copy(company = c) }
+
+  val lens: Lens[Employee, String] =
+    _company composeLens _address composeLens _street composeLens _name
+
+  test("get") {
+    check(forAll { (employee: Employee) =>
+      val expected = employee.company.address.street.name
+      val actual   = lens get employee
+      actual == expected
+    })
+  }
+
+  test("set") {
+    check(forAll { (employee: Employee) =>
+      val employee2 = lens.set("fleet")(employee)
+      employee2 == Employee(Company(Address(Street("fleet"))))
+    })
+  }
+
+  test("modify") {
+    check(forAll { (employee: Employee) =>
+      val name      = lens get employee
+      val employee2 = lens.modify(_.capitalize)(employee)
+      employee2 == Employee(Company(Address(Street(name.capitalize))))
+    })
   }
 }
