9126e7ceeebba0b658e41e275d9a82d6c63efebf
0918nobita <nobita.0918@gmail.com>
2020-06-27 13:31:26 +0900

    :recycle: for yield 式を用いて Runtime をリファクタリングした

--- src/main/scala/vision/kodai/xemime/Runtime.scala
+++ src/main/scala/vision/kodai/xemime/Runtime.scala
@@ -9,14 +9,18 @@ object Runtime {
       case IntConst(_, value)    => Right(XeInt(value))
       case DoubleConst(_, value) => Right(XeDouble(value))
       case BinExpr(_, lhs, AddOp(_), rhs) =>
-        run(lhs).flatMap {
-          case lhsVal: XeInt =>
-            run(rhs).flatMap {
-              case rhsVal: XeInt => Right(XeInt(lhsVal.value + rhsVal.value))
-              case _             => Left("TypeError")
-            }
-          case _ => Left("TypeError")
-        }
+        for {
+          lhsVal <- run(lhs)
+          lhsInt <- lhsVal match {
+            case XeInt(v) => Right(v)
+            case _        => Left("TypeError")
+          }
+          rhsVal <- run(rhs)
+          rhsInt <- rhsVal match {
+            case XeInt(v) => Right(v)
+            case _        => Left("TypeError")
+          }
+        } yield XeInt(lhsInt + rhsInt)
       case _ => Left("Unimplemented")
     }
 }
