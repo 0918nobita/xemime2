a449f95b61cd04f5b74a027db3faeb7c1c734ff2
0918nobita <nobita.0918@gmail.com>
2020-07-01 16:16:20 +0900

    :sparkles: macOS でのみ Dock アイコンの設定が行われるようにした

--- core/src/main/scala/vision/kodai/xemime/AppPreloader.scala
+++ core/src/main/scala/vision/kodai/xemime/AppPreloader.scala
@@ -1,20 +1,25 @@
 package vision.kodai.xemime
 
-import com.sun.glass.ui.Application
 import javafx.application.Preloader
 import javafx.stage.Stage
 import javax.swing.ImageIcon
 
-class AppPreloader extends Preloader {
-  override def start(stage: Stage): Unit =
-    System.getProperty("os.name") match {
-      case name if name.startsWith("Linux") =>
-        Application.GetApplication().setName("Xemime")
+import vision.kodai.xemime.macros._
 
-      case name if name.startsWith("Mac") =>
+class AppPreloader extends Preloader {
+  override def start(stage: Stage): Unit = {
+    osDependent(
+      "Linux", {
+        com.sun.glass.ui.Application.GetApplication().setName("Xemime")
+      }
+    )
+
+    osDependent(
+      "Mac", {
         val url   = getClass.getResource("/example.png")
         val image = new ImageIcon(url).getImage
-      // TODO: マクロを用いて、非 macOS 環境で com.apple.eawt パッケージをビルドしないようにする
-      // com.apple.eawt.Application.getApplication.setDockIconImage(image)
-    }
+        java.awt.Taskbar.getTaskbar.setIconImage(image)
+      }
+    )
+  }
 }
